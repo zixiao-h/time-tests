d6053215703f99f52266fcc9f3ad7070f277f0cd
Zixiao Hu <zixiao.hu.09@gmail.com>
2024-10-25 16:45:10 +0100

    added unit test for both multiple intervals each

--- test_times.py
+++ test_times.py
@@ -14,3 +14,11 @@ def test_no_overlap():
     result = compute_overlap_time(large, short)
     expected = []
     assert result == expected
+
+
+def test_multiple_intervals_each():
+    large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00", 3)
+    short = time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 2, 60)
+    result = compute_overlap_time(large, short)
+    expected = [('2010-01-12 10:30:00', '2010-01-12 10:37:00'), ('2010-01-12 10:38:00', '2010-01-12 10:40:00'), ('2010-01-12 10:40:00', '2010-01-12 10:45:00')]
+    assert result == expected
